a7a0d07fde6ffa7106106b7b22f5ddcc8c45e26a
Nicole Hall <nhall@pairin.com>
2018-05-08 11:09:17 -0600

    starting this exercise over b/c the last one was buggy and I think I'll learn better. Build and burn as they say

---
+++ src/components/CartFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartFooter = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a calssName="navbar-brand" href="https://www.cnn.com">&copy; 2018</a>
+    </nav>
+  )
+}
+
+export default CartFooter
